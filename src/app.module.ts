c41b2de1419684ac0704c7f5d5ab96c472d95ebd
Rameshwor <r.sresta07@gmail.com>
2025-04-06 20:55:38 +0545

    [Add]: Database implementation and base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './config/database.config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { SeedsService } from './database/seeds/seeds.service';
+import { User } from './user/entities/user.entity';
+import { UserService } from './user/user.service';
+import { BcryptService } from './auth/bcryptjs/bcrypt.service';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import MailConfig from './config/mail.config';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(AppDataSource.options),
+    // MailerModule.forRoot(MailConfig),
+
+    TypeOrmModule.forFeature([User]),
+    AuthModule,
+    UserModule,
+    HealthCheckModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SeedsService, UserService, BcryptService, Pagination],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private readonly seedsService: SeedsService) {}
+
+  async onModuleInit() {}
+}
