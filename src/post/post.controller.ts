7e2179904f88d75fe3013969554d79d842b777e0
Rameshwor <r.sresta07@gmail.com>
2025-04-20 18:58:44 +0545

    [Add]: Create api for Post blogs

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,36 +1,61 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { PostService } from './post.service';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
-import {ApiTags} from "@nestjs/swagger";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from "@nestjs/common";
+import { PostService } from "./post.service";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
+import { ApiTags } from "@nestjs/swagger";
+import { ResponseMessage } from "../core/decorators/response.decorator";
+import { CREATED } from "../auth/auth.constant";
 
 @ApiTags("Post")
-@Controller('post')
+@Controller("/post")
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  @Post()
-  create(@Body() createPostDto: CreatePostDto) {
+  //* Create New Student
+  @Post("/create")
+  @ResponseMessage(CREATED)
+  async create(@Body() createPostDto: CreatePostDto) {
     return this.postService.create(createPostDto);
   }
 
-  @Get()
+  //* Get all post
+  @Get("/all")
+  @UseInterceptors(ClassSerializerInterceptor)
   findAll() {
     return this.postService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
+  //* Get active post
+  @Get("/active")
+  @UseInterceptors(ClassSerializerInterceptor)
+  findActive() {
+    return this.postService.findActive();
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
+  //* Get post details
+  @Get("/:slug")
+  @UseInterceptors(ClassSerializerInterceptor)
+  findOne(@Param("slug") slug: string) {
+    return this.postService.findOne(slug);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postService.remove(+id);
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postService.update(id, updatePostDto);
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.postService.remove(id);
   }
 }
