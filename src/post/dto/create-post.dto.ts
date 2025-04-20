cef744c4636e1270c49439bc60467577700cb5fb
Rameshwor <r.sresta07@gmail.com>
2025-04-20 18:59:16 +0545

    [Fix]: DTO of post blog

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,48 @@
-export class CreatePostDto {}
+import {
+  IsArray,
+  IsBoolean,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
+
+export class CreatePostDto {
+  @IsString()
+  @ApiProperty({ example: "A New Day" })
+  @Transform(({ value }) => value.trim())
+  title: string;
+
+  @IsString()
+  @ApiProperty({
+    example:
+      "<p>A New Month in the life of all these people that are traveling to the world</p>",
+  })
+  @Transform(({ value }) => value.trim())
+  description: string;
+
+  @IsString()
+  @ApiProperty({ example: "/L029.jpg" })
+  image: string;
+
+  @IsBoolean()
+  @ApiProperty({ example: true })
+  status: boolean;
+
+  @IsArray()
+  @IsUUID("all", { each: true })
+  @Transform(({ value }) => (Array.isArray(value) ? value : [value]))
+  @ApiProperty({
+    example: ["98eec7cf-8b52-4f02-aac8-d17b886a901a"],
+  })
+  tagIds: string[];
+
+  @IsArray()
+  @IsUUID("all", { each: true })
+  @Transform(({ value }) => (Array.isArray(value) ? value : [value]))
+  @ApiProperty({
+    example: ["69193f2c-9f02-4502-9560-06262c9303f9"],
+  })
+  userIds: string[];
+}
