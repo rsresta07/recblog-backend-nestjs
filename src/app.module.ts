69a8f72102fef098afb9d8506ccffd5bdcd720d8
Rameshwor <r.sresta07@gmail.com>
2025-11-13 06:37:33 +0545

    feat(seeder): implement database seeding on application start
    Introduces a `SeederService` to handle initial database population.
    This service is now provided in the `AppModule` and its `run` method is called during `onModuleInit` to ensure essential data is present when the application starts.
    Removes previous `SeedsService` and related unused imports.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,9 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppDataSource } from "./config/database.config";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
-import { SeedsService } from "./database/seeds/seeds.service";
 import { User } from "./user/entities/user.entity";
 import { UserService } from "./user/user.service";
 import { BcryptService } from "./auth/bcryptjs/bcrypt.service";
-import { Pagination } from "nestjs-typeorm-paginate";
 import { HealthCheckModule } from "./health-check/health-check.module";
 import { PostModule } from "./post/post.module";
 import { TagsModule } from "./tags/tags.module";
@@ -22,12 +20,12 @@ import { Follow } from "./user/entities/follow.entity";
 import { Comment } from "./post/entities/comment.entity";
 import { Tag } from "./tags/entities/tag.entity";
 import { RecommendationServiceModule } from "./recommendation/recommendation.module";
+import { SeederService } from "./seeder/seeder.service";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(AppDataSource.options),
-
-    TypeOrmModule.forFeature([Tag, Follow, PostLike, Comment, User]),
+    TypeOrmModule.forFeature([Tag, Follow, PostLike, Comment, User, Post]),
     AuthModule,
     UserModule,
     HealthCheckModule,
@@ -39,10 +37,17 @@ import { RecommendationServiceModule } from "./recommendation/recommendation.mod
     RecommendationServiceModule,
   ],
   controllers: [AppController],
-  providers: [AppService, SeedsService, UserService, BcryptService, Pagination],
+  providers: [
+    AppService,
+    UserService,
+    BcryptService,
+    SeederService, // <-- provide SeederService here
+  ],
 })
 export class AppModule implements OnModuleInit {
-  constructor(private readonly seedsService: SeedsService) {}
+  constructor(private readonly seederService: SeederService) {}
 
-  async onModuleInit() {}
+  async onModuleInit() {
+    await this.seederService.run();
+  }
 }
